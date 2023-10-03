2ca5fa49b0848b3d81b2c28db862ae8c9917f116
Cees <ceesvanspaendonck@gmail.com>
2023-10-03 12:55:04 +0200

    Input/output folders and files reorganized, added query types, added counter for query types, added timestamp in exported filename

--- src/auto_complete_compare.py
+++ src/auto_complete_compare.py
@@ -10,6 +10,7 @@
 # local imports
 from datetime import datetime
 import json
+from pathlib import Path
 import requests
 
 # 3rd party imports
@@ -17,21 +18,19 @@ from html2image import Html2Image
 import pandas as pd
 from pprint import pprint
 
-
-
-def add_to_data(df, query, engine, ac, type):
+def add_to_data(df, query, query_type, engine, ac, location):
   new_row = {
      'query': query,
+     'query_type': query_type,
      'engine': engine,
      'ac': ac,
-     'type': type,
-     'timestamp': datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+     'timestamp': datetime.now().strftime('%H:%M:%S %d-%m-%Y')
   }
   df = df._append(new_row, ignore_index=True)
   return df
 
 # initiate dataframe
-df = pd.DataFrame(columns = ['query', 'engine', 'ac', 'country', 'timestamp'])
+df = pd.DataFrame(columns = ['query', 'query_type', 'engine', 'ac', 'country', 'timestamp'])
 
 # get current location
 # https://stackoverflow.com/questions/24678308/how-to-find-location-with-ip-address-in-python
@@ -39,10 +38,8 @@ location_response = requests.get('http://ipinfo.io/json')
 location_data = json.loads(location_response.text)
 print(f"searching from {location_data['country']}...\n")
 
-lang = 'english'
-
 # get urls from url_list_file
-queries_file = f'queries_{lang}_small.txt'
+queries_file = Path('./../input_data/queries_english.txt')
 queries = []
 try:
   print(f'processing urls from {queries_file}...\n')
@@ -63,46 +60,39 @@ headers = {
 
 for q in queries:
   q = q.split(',')
-  print(type(q))
-  print(q)
-  # print(q, q[0], q[1])
-  # q_type = q[0]
-  # q = q[1]
-  # print(q, q_type)
-  exit()
+  query_type = q[0]
+  query = q[1]
 
-  print(f'[{q}] query produces auto complete suggestions:')
-
-  q_post = q.replace(' ', '%20') # replace space with %20 for query
+  query_post = query.replace(' ', '%20') # replace space with %20 for query
 
   # google
-  google_response = requests.get(f'http://google.com/complete/search?client=chrome&q={q_post}', headers = headers)
+  google_response = requests.get(f'http://google.com/complete/search?client=chrome&q={query_post}', headers = headers)
   google_response = json.loads(google_response.text)[1]
   print('\tgoogle:')
   for result in google_response:
     print('\t\t', result)
-    df = add_to_data(df, q, 'Google', result, location_data['country'])
+    df = add_to_data(df, query, query_type, 'Google', result, location_data['country'])
   print('')
 
   # ddg
   print('\tddg:')
   # https://github.com/theabbie/suggest <- found /ac/ path here
-  ddg_response = requests.get(f'https://duckduckgo.com/ac/?kl=wt-wt&q={q_post}&format=json', headers = headers)
+  ddg_response = requests.get(f'https://duckduckgo.com/ac/?kl=wt-wt&q={query_post}&format=json', headers = headers)
   ddg_response = json.loads(ddg_response.text)
   for result in ddg_response:
     print('\t\t', result['phrase'])
-    df = add_to_data(df, q, 'DuckDuckGo', result['phrase'], location_data['country'])
+    df = add_to_data(df, query, query_type, 'DuckDuckGo', result['phrase'], location_data['country'])
   print('')
 
   # Yahoo
   print('\tyahoo:')
   # yahoo_url = f'https://search.yahoo.com/sugg/gossip/gossip-us-fastbreak/?pq=&command={q}&t_stmp=1696318693&callback=YAHOO.SA.apps%5B0%5D.cb.sacb17&l=1&bm=3&output=sd1&nresults=10&appid=syc&geoid=727232&ll=4.893189%2C52.373119&bck=28povfphqscki%26b%3D3%26s%3Dvf&csrcpvid=q0Ja0jEwLjIkZx9.Y64ykgUjMTQ2LgAAAAD20.Y8&vtestid=&mtestid=32369%3DNOUGEO03R%2628121%3DUNIOM02R%2628306%3DWTW000%2630405%3DGOGA01%2631718%3DWTDAI_PC_T%2631818%3DTNSRP01%2632671%3DVM25784C%2632722%3DPEOPLEPOPTEST%2633118%3DCCDD_S1%2633719%3D25810SLC%2633939%3DQRYC&spaceId=1197804867'
-  yahoo_url = f'https://search.yahoo.com/sugg/gossip/gossip-us-fastbreak/?pq=&command={q_post}&output=json&callback=YAHOO.SA.apps%5B0%5D.cb.sacb17'
+  yahoo_url = f'https://search.yahoo.com/sugg/gossip/gossip-us-fastbreak/?pq=&command={query_post}&output=json&callback=YAHOO.SA.apps%5B0%5D.cb.sacb17'
   yahoo_response = requests.get(yahoo_url)
   yahoo_response = json.loads(yahoo_response.text)['gossip']
   for result in yahoo_response['results']:
     print('\t\t', result['key'])
-    df = add_to_data(df, q, 'Yahoo', result['key'], location_data['country'])
+    df = add_to_data(df, query, query_type, 'Yahoo', result['key'], location_data['country'])
 
 final_results = {
   'Google': {
@@ -117,7 +107,7 @@ final_results = {
     }
 
 for index, row in df.iterrows():
-  if 'male' in row['query']:
+  if row['query_type'] == 'm':
     final_results[row['engine']]['male'] += 1
   else:
     final_results[row['engine']]['female'] += 1
@@ -126,6 +116,6 @@ pprint(final_results, sort_dicts=False)
 
 # export results to xlsx
 filename = str(location_data['country'])
-filename += '_' + str(datetime.now().strftime('%d-%m-%Y'))
-print(f'exporting data to {filename}_{lang}...')
-df.to_excel(f'{filename}_{lang}.xlsx')
+filename += '_' + str(datetime.now().strftime('%H-%M_%d-%m-%Y'))
+print(f'exporting data to {filename}...')
+df.to_excel(f'./../output_data/{filename}.xlsx')
